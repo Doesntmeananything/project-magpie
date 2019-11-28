0fc0c64ed2a4d31494127cd3addb6997d5e43a1a
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-11-28 23:15:31 +0300

    Replace .js files with .tsx

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,13 +1,10 @@
-import React from 'react'
-import Link from 'next/link'
+import React from "react";
+import Link from "next/link";
 
 const links = [
-  { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/zeit/next.js', label: 'GitHub' },
-].map(link => {
-  link.key = `nav-link-${link.href}-${link.label}`
-  return link
-})
+  { href: "https://zeit.co/now", label: "ZEIT" },
+  { href: "https://github.com/zeit/next.js", label: "GitHub" }
+];
 
 const Nav = () => (
   <nav>
@@ -17,8 +14,8 @@ const Nav = () => (
           <a>Home</a>
         </Link>
       </li>
-      {links.map(({ key, href, label }) => (
-        <li key={key}>
+      {links.map(({ href, label }) => (
+        <li key={`nav-link-${href}-${label}`}>
           <a href={href}>{label}</a>
         </li>
       ))}
@@ -51,6 +48,6 @@ const Nav = () => (
       }
     `}</style>
   </nav>
-)
+);
 
-export default Nav
+export default Nav;
