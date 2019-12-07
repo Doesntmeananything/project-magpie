c256763b3c6a0cf63d58c399618beafb4fe672b5
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-12-07 22:24:03 +0300

    Add react-grid-layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import Layout from "../components/layout";
-import { PersonCard } from "../components/personCard";
+import { Board } from "../components/board";
 
 const Home = () => (
   <Layout>
-    <PersonCard
-      avatarUrl="https://avatars3.githubusercontent.com/u/11068840"
-      name="Olga Isakova"
-      jobTitle="Software Engineer"
-      currentSkill="Angular"
-    />
+    <div style={{ height: "100vh" }}>
+      <Board />
+    </div>
   </Layout>
 );
 
